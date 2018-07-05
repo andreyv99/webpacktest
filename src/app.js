a09954edae089672e6dd2f49a484504df0159186
John Doe <johndoe@example.com>
2018-07-05 21:48:15 +0300

    css loader

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import toUpperCase from './mod/utils.js';
+import './app.css';
 var arr = ['a', 'b', 'c'].map(toUpperCase);
 document.querySelector("#content").innerHTML = "conteneeeet";
 console.log(arr);
